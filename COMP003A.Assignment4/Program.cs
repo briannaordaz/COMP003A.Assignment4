c7f258d80e8a5ffcea71c3582c9164ca89401c99
briannaordaz <briannaordaz5@gmail.com>
2026-02-06 21:44:01 -0800

    completed step 1, menu driven program flow

---
+++ COMP003A.Assignment4/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+
+
+
+        int choice;
+        
+        Console.Write("1. Start/continue Routine\n2. View Progress\n3. Exit");
+        
+        do
+        {
+           
+            Console.WriteLine("\nEnter choice: ");
+            choice = int.Parse(Console.ReadLine());
+            
+            
+        }
+        while (choice == 1 || choice == 2);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
+
+
+ 
